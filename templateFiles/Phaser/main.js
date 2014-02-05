af74225d0bada6848e04bf5600b09315825942b8
Otto Robba <OttoRobba@users.noreply.github.com>
2014-02-05 03:43:42 -0200

    Create main.js

---
+++ templateFiles/Phaser/main.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+}
+
+
+function update() {
+
+}
+
+function create() {
+
+}
