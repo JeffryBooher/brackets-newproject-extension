d88c9acd13b4d52a09acc7596a7b1c6441e70518
unknown <Zivolof@ITPC010.progetto2000.intra>
2014-05-28 09:13:48 +0200

    Added italian translation

---
+++ nls/it/strings.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+// Italian - root strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+  "MY_DOCUMENTS"                      : "Miei Documenti",
+  "DOCUMENTS"                         : "Documenti",
+  "MENU_TITLE"                        : "Nuovo Progetto…",
+  "NEW_PROJECT_BASE_NAME"             : "Senza titolo-",
+  "CHANGE"                            : "Cambia…",
+  "PROJECT_DIRECTORY_lABEL"           : "Cartella Principale:",
+  "CHOOSE_TEMPLATE_LABEL"             : "Template:",
+  "PROJECT_NAME_LABEL"                : "Nome:",
+  "DIALOG_TITLE"                      : "Crea Nuovo Progetto",
+  "NO_TEMPLATE_OPTION_LABEL"          : "Nessuno (Progetto Vuoto)",
+  "ERROR_NOT_A_DIRECTORY"             : "Impossibile scrivere su {1} perché non è una directory valida, non è raggiungibile o non esiste.",
+  "ERROR_UNABLE_TO_WRITE_DIRECTORY"   : "Impossibile scrivere su {1} (Errore {0})",
+  "INVALID_PROJECT_NAME"              : "Nome del Progetto non valido",
+  "INVALID_PROJECT_NAME_MESSAGE"      : "I nomi dei progetti non possono contenere i seguenti caratteri: /?*:;{}<>\\| od usare una qualunque parola riservata dal sistema.",
+  "ONE_OR_MORE_TEMPLATE_FILES_FAILED" : "Impossibile copiare tutti i file del template nella cartella di destinazione. Una o più operazioni di copia fallite."
+});
