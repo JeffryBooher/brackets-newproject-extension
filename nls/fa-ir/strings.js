a6acbc33e1ab280386f61c700a1da5dbc7645b82
pooya parsa dadashi <Developers@datamweb.ir>
2014-11-12 22:35:20 +0330

    Create strings.js
    Add to Persian (Farsi | فارسی) translation file.

---
+++ nls/fa-ir/strings.js
@@ -0,0 +1,43 @@
+/*
+* Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*/
+// farsi|persian|فارسی -
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+define({
+"MY_DOCUMENTS" : "اسناد من",
+"DOCUMENTS" : "اسناد",
+"MENU_TITLE" : "پروژه جدید ...",
+"NEW_PROJECT_BASE_NAME" : "بدون عنوان-",
+"CHANGE" : "تغییر…",
+"PROJECT_DIRECTORY_lABEL" : "پوشه اصلی:",
+"CHOOSE_TEMPLATE_LABEL" : "الگو:",
+"PROJECT_NAME_LABEL" : "نام:",
+"DIALOG_TITLE" : "ایجاد پروژه جدید",
+"NO_TEMPLATE_OPTION_LABEL" : "هچی (پروژه خالی).",
+"ERROR_NOT_A_DIRECTORY" : "ناتوان در نوشتن {1} چرا که دایرکتوری نا معتبر میباشد, و یا ظرفیت در دسترس نیست.",
+"ERROR_DIRECTORY_ALREADY_EXISTS" : "ناتوان در ایجاد دایرکتوری پروژه {1} چرا که قبلا موجود میباشد.",
+"ERROR_UNABLE_TO_WRITE_DIRECTORY" : "ناتوان در نوشتن {1} (خطا {0})",
+"INVALID_PROJECT_NAME" : "نام پروژه نا معتبر است.",
+"INVALID_PROJECT_NAME_MESSAGE" : "نام پروژه نباید شامل کارکترهای باشد: /?*:;{}<>\\| یا استفاده کنید از  کلمه مناسب.",
+"ONE_OR_MORE_TEMPLATE_FILES_FAILED" : "کپی تمام فایلهای موجود در پوشه الگوها به مقصد امکان پذیر نمیباشد.عملیات کپی یک ویا بیشتر از یک فایل موفق نمی باشد."
+});
